af36703d7c703131d96c59258e4333758d26b6e1
simone menichetti <simon3menichettui95@gmail.com>
2024-07-05 17:11:26 +0200

    build:( add and initialize export AppFooter components )

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <script >
 import AppHeader from './components/AppHeader.vue'
+import AppFooter from './components/AppFooter.vue'
 
 export default{
   name:'App',
   components:{
     AppHeader,
+    AppFooter,
   }
 }
 </script>
 
 <template>
   <AppHeader/>
+  <AppFooter/>
 </template>
 
 <style lang="scss">
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,9 @@
+<script>
+export default{
+    name:'AppFooter',
+}
+</script>
+
+<style lang="scss"scoped>
+
+</style>
