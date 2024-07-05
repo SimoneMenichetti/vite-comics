99b7a75e18d9fb0f4cf567c11e7584c76077ecc6
simone menichetti <simon3menichettui95@gmail.com>
2024-07-05 16:16:43 +0200

    build:( add path mixins, variables and general scss and add components for initialize AppHeader) feat:( export and use variables and general scss)

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,51 @@
+<script>
+export default{
+    name:'AppHeader',
+}
+</script>
+
+<template>
+    <header>
+        <a href="/" class="brand">
+            <img src="../assets/img/dc-logo.png" alt="logo png">
+        </a>
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">characters</a>
+                </li>
+                <li>
+                    <a href="#">comics</a>
+                </li>
+                <li>
+                    <a href="#">movies</a>
+                </li>
+                <li>
+                    <a href="#">tv</a>
+                </li>
+                <li>
+                    <a href="#">games</a>
+                </li>
+                <li>
+                    <a href="#">collectibles</a>
+                </li>
+                <li>
+                    <a href="#">videos</a>
+                </li>
+                <li>
+                    <a href="#">fans</a>
+                </li>
+                <li>
+                    <a href="#">news</a>
+                </li>
+                <li>
+                    <a href="#">shop</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<style lang="scss"scoped>
+
+</style>
