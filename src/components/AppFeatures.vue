1c6746f380305129eb241e125c890ffc1134bf99
simone menichetti <simon3menichettui95@gmail.com>
2024-07-07 21:35:33 +0200

    feat: features( add data object and add v for for dinamize template)

--- src/components/AppFeatures.vue
+++ src/components/AppFeatures.vue
@@ -1,12 +1,49 @@
 <script>
 export default{
     name:'AppFeatures',
-}
+    data() {
+    return {
+      featuresData: [
+        {
+          imgSrc: "/public/img/buy-comics-digital-comics.png",
+          imgAlt: "Digital Comics",
+          text: "DIGITAL COMICS"
+        },
+        {
+          imgSrc: "/public/img/buy-comics-merchandise.png",
+          imgAlt: "DC Merchandise",
+          text: "DC MERCHANDISE"
+        },
+        {
+          imgSrc: "/public/img/buy-comics-shop-locator.png",
+          imgAlt: "Subscription",
+          text: "SUBSCRIPTION"
+        },
+        {
+          imgSrc: "/public/img/buy-comics-shop-locator.png",
+          imgAlt: "Comic Shop Locator",
+          text: "COMIC SHOP LOCATOR"
+        },
+        {
+          imgSrc: "/public/img/buy-dc-power-visa.svg",
+          imgAlt: "DC Power Visa",
+          text: "DC POWER VISA"
+        }
+      ]
+    };
+  }
+};
+
 </script>
 
 <template>
       <section class="features">
-        <div class="feature">
+        <!-- inizializiamo v for per iterare nel data e dinamicizz img e p -->
+        <div v-for="feature in featuresData" :key="feature.text" class="feature">
+            <img :src="feature.imgSrc" :alt="feature.imgAlt">
+            <p>{{ feature.text }}</p>
+        </div>
+        <!-- <div class="feature">
             <img src="../assets/img/buy-comics-digital-comics.png" alt="Digital Comics">
             <p>DIGITAL COMICS</p>
         </div>
@@ -25,7 +62,7 @@ export default{
         <div class="feature">
             <img src="../assets/img/buy-dc-power-visa.svg" alt="DC Power Visa">
             <p>DC POWER VISA</p>
-        </div>
+        </div> -->
     </section>
 </template>
 
