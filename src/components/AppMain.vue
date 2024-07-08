eb753a8cc4e902910ac9b8e50c5a3800ce0f9a5d
simone menichetti <simon3menichettui95@gmail.com>
2024-07-08 13:48:07 +0200

    build: AppMain( add cards structure template and style )

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -5,11 +5,57 @@ export default{
 </script>
 
 <template>
-    <main class="main-content">
-        <div class="content">
-            <!-- Content goes here -->
-            <h3> --> Content goes here <--</h3>
+    <main>
+        <div class="container">
+        <div class="card">
+            <img src="https://www.coverbrowser.com/image/action-comics/1-1.jpg" alt="Action comics">
+            <p>ACTION COMICS</p>
         </div>
+        <div class="card">
+            <img src="https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg" alt="American Vampire">
+            <p>AMERICAN VAMPIRE 1976</p>
+        </div>
+        <div class="card">
+            <img src="https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg" alt="acquaman">
+            <p>AQUAMAN</p>
+        </div>
+        <div class="card">
+            <img src="https://d29xot63vimef3.cloudfront.net/image/batgirl/1-1.jpg" alt="Batgirl">
+            <p>BATGIRL</p>
+        </div>
+        <div class="card">
+            <img src="https://static.posters.cz/image/750/locandine-film-in-plexiglass-batman-prowl-comic-cover-i69653.jpg" alt="Batman">
+            <P>BATMAN</P>
+        </div>
+        <div class="card">
+            <img src="https://static.wikia.nocookie.net/marvel_dc/images/5/50/Batman_Beyond_v.1_1.jpg" alt="batmanbeyond">
+            <P>BATMAN BEYOND</P>
+        </div>
+        <div class="card">
+            <img src="https://static.wikia.nocookie.net/marvel_dc/images/0/0d/Batman_Superman_Vol_1_1.jpg" alt="SupermanVol1">
+            <P>BATMAN/SUPERMAN</P>
+        </div>
+        <div class="card">
+            <img src="https://static.wikia.nocookie.net/marvel_dc/images/c/cf/Batman_Superman_Annual_Vol_2_1.jpg" alt="Batman Superman Vol2">
+            <P>BATMAN/SUPERMAN ANNUAL</P>
+        </div>
+        <div class="card">
+            <img src="https://static.wikia.nocookie.net/marvel_dc/images/5/54/Batman_The_Joker_War_Zone_Vol_1_1.jpg" alt="Batman The joker war vol1">
+            <P>BATMAN: THE JOKER WAR ZONE</P>
+        </div>
+        <div class="card">
+            <img src="https://static.wikia.nocookie.net/marvel_dc/images/6/64/Batman_Three_Jokers_Collected.jpg" alt="batman three">
+            <P>BATMAN: THREE JOKERS</P>
+        </div>
+        <div class="card">
+            <img src="https://static.wikia.nocookie.net/marvel_dc/images/f/f8/Batman_White_Knight_Presents_Harley_Quinn_Vol_1_1.jpg" alt="Harley Quinn">
+            <P>BATMAN: WHITE KNIGHT PRESENTS: HARLEY QUINN</P>
+        </div>
+        <div class="card">
+            <img src="https://static.wikia.nocookie.net/marvel_dc/images/c/c8/Catwoman_Vol_2_1.jpg" alt="CatWoman vol 2">
+            <P>CATWOMAN</P>
+        </div>
+    </div>
     </main>
 </template>
 
@@ -19,19 +65,47 @@ export default{
 
 
 // sezione MAIN
-.main-content {
-    display: flex;
-    justify-content: start;
-    align-items: center;
-    height: 150px; 
-    background-color: black;
-   
+main{
+    background-color: #1C1C1C;
+    .container {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        height: 450px;
+        gap: 20px;
+        padding: 20px;
+    }
 
-    .content {
+    .card {
+        width: 150px;
+        height: 150px;
+        padding-bottom: 20px;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
         text-align: center;
-        font-size: 16px;
-        margin-left: 10rem;
+        font-size: 18px;
+        color: #333333;
+
+        p{
+            font-size: 10px;
+            color: #fff;
+            text-align: left;
+            margin-top:10px;
+        }
+    }
+
+    .card img{
+      
+        width: 100%;
+        height: 100%;
+        background-repeat: no-repeat;
+        background-size: cover;
+        display: block;
         cursor: pointer;
+     
+    }
+
+    .card img:hover {
+        box-shadow: 0 4px 8px rgba(1, 81, 172, 0.8);
     }
 }
 
